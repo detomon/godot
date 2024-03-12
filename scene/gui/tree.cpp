5c5902cc1f54c7cacb559380b6936ee784963b19
detomon <simon@monoxid.ch>
2024-03-12 16:31:08 +0100

    Fix `ui_down` in `Tree` when no item is selected

--- scene/gui/tree.cpp
+++ scene/gui/tree.cpp
@@ -3382,6 +3382,7 @@ void Tree::_go_down() {
 		if (root) {
 			next = hide_root ? root->get_next_visible() : root;
 		}
+		selected_col = 0;
 	} else {
 		next = selected_item->get_next_visible();
 	}
